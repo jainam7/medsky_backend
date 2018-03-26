ae89306c8da64030b10105e5fc3b5f616661acf6
malavshah9 <mdshah9574@gmail.com>
2018-03-26 22:40:46 +0530

    Heroku 1st push

--- dbconnection.js
+++ dbconnection.js
@@ -1,8 +1,12 @@
 var mysql=require('mysql');
 var connection=mysql.createPool({
-    host:'localhost',
-    user:'root',
-    password:'',
-    database:'medsky'
+    host:'sql12.freemysqlhosting.net',
+    user:'sql12228778',
+    password:'dSUWZ6DakP',
+    database:'sql12228778'
+    // host:'localhost',
+    // user:'root',
+    // password:'',
+    // database:'medsky'
 });
 module.exports=connection;
