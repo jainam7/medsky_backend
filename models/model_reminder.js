f8e1f6fb6e875d73577dd59afa166baf70c7ef14
malavshah9 <mdshah9574@gmail.com>
2018-03-27 16:45:29 +0530

    Reminder Delete Added

--- models/model_reminder.js
+++ models/model_reminder.js
@@ -9,10 +9,12 @@ var reminder={
         return db.query("select * from reminder_tbl where pk_rem_id=?",[id],callback);
     },
 
+    deleterem:function(id,callback){
+        return db.query("delete from reminder_tbl where pk_rem_id=?",[id],callback);
+    },
+
     
     insert_reminder:function(Remind,callback){
-
-        
         return db.query("insert into reminder_tbl values(?,?,?,?,?,?)",[Remind.pk_rem_id,Remind.fk_usr_email_id,Remind.rem_title,Remind.rem_desc,Remind.start_date,Remind.end_date],callback);
     },
     
--- routes/routes_reminder.js
+++ routes/routes_reminder.js
@@ -43,5 +43,19 @@ router.post('/',function(req,res,next){
             }
         });
     });
+    router.put('/:id',function(req,res,next){
+        reminder.deleterem(req.params.id,req.body,function(err,rows){
+         
+        if(err)
+          {
+          res.json(err);
+          }
+          else
+          {
+          res.json(rows);
+          }
+        });
+        });
+         
     
 module.exports=router;
